b711926e142267c04b12cd7d8227a396395f2dfd
Alisher Aliev <alisher.aliev@tietoevry.com>
2022-11-08 18:37:52 +0200

    Add constraint to prevent empty title and author records;

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE books (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title varchar(255) NOT NULL UNIQUE,
     author varchar(255) NOT NULL,
-    description varchar(500) 
+    description varchar(500),
+    CONSTRAINT chk_title_and_author_valid CHECK (LENGTH(TRIM(title)) > 0 AND LENGTH(TRIM(author)) > 0)
 ) DEFAULT CHARSET=utf8mb4; -- setting character set just in case (or if you do the execution in your own DB instance)
 
 /*
